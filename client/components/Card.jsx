7659753ac2d03e22242454feed522ead65d13466
James Koay <88022942+james-koay@users.noreply.github.com>
2022-11-21 08:41:02 +1300

    added drag handlers

--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -5,6 +5,37 @@ import { getPokemonById } from '../apiClient'
 function Card(props) {
   const [pokemonData, setPokemonData] = useState({})
   const [isPokemonLoading, setIsPokemonLoading] = useState(true)
+  const [dragInfo, setDragInfo] = useState({ x: 0, y: 0 })
+
+  // function handleDragUp(props.cardEffect){
+  //   return cardEffect()
+  // }
+
+  function handleDragStart(event, info) {
+    console.log(info.point.x, info.point.y)
+    console.log(event)
+    setDragInfo({
+      x: info.point.x,
+      y: info.point.y,
+    })
+    console.log(dragInfo.x, dragInfo.y)
+  }
+
+  function handleDragEnd(event, info) {
+    console.log(dragInfo.x, dragInfo.y)
+    console.log(info.point.x, info.point.y)
+    console.log(event)
+    let changeX = dragInfo.x - info.point.x
+    let changeY = dragInfo.y - info.point.y
+    if (changeY > 150) {
+      console.log('card is played')
+      // props.cardEffect()
+    } else if (changeX > 100) {
+      console.log('card moved left')
+    } else if (changeX < -100) {
+      console.log('card moved right')
+    }
+  }
 
   useEffect(() => {
     setIsPokemonLoading(true)
@@ -31,18 +62,12 @@ function Card(props) {
       drag
       dragConstraints={{ top: 0, bottom: 0, left: 0, right: 0 }}
       dragDirectionLock
-      onDragEnd={(event, info) => {
-        console.log(info.point.x, info.point.y)
-      }}
-      onDragStart={(event, info) => {
-        console.log(info.point.x, info.point.y)
-      }}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
     >
       <div className="card-frame">
         <img
-          onDragStart={(event) => {
-            event.preventDefault()
-          }}
+          draggable="false"
           // src={pokemonData.sprites.front_default}
           src="images/flanged-mace.png"
           alt="cardimage"
